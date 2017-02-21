c9c60bf7f4d9d65fc29f3fd2dd3a5f4f9137a99b
gcamp <guillaume.campagna@gmail.com>
2017-02-21 11:37:20 -0500

    Fix warnings

--- src/pavlov/LzmaEnc.c
+++ src/pavlov/LzmaEnc.c
@@ -1913,11 +1913,13 @@ static SRes LzmaEnc_CodeOneBlock(CLzmaEnc *p, Bool useLimits, UInt32 maxPackSize
 static SRes LzmaEnc_Alloc(CLzmaEnc *p, UInt32 keepWindowSize, ISzAlloc *alloc, ISzAlloc *allocBig)
 {
   UInt32 beforeSize = kNumOpts;
-  Bool btMode;
+  
   if (!RangeEnc_Alloc(&p->rc, alloc))
     return SZ_ERROR_MEM;
+  
+  #ifdef COMPRESS_MF_M
+  Bool btMode;
   btMode = (p->matchFinderBase.btMode != 0);
-  #ifdef COMPRESS_MF_MT
   p->mtMode = (p->multiThread && !p->fastMode && btMode);
   #endif
 
